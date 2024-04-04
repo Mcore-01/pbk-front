e0d728f3524130b94a0f32ff13ebe1168be678be
Mcore-01 <dassd2123@gmail.com>
2024-04-04 21:33:06 +0500

    feat: добавил настройки файл настройки proxy.config.json

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,8 +10,8 @@ export class AuthService {
     constructor(private http: HttpClient) { }
     //Запрашиваем у сервера токен, если статус 200, то добавляем токен в локальное хранилище
     createToken(login: string, password:string){
-        const data = {username: login, password: password};
-        return  this.http.post<any>("http://localhost:57291/api/User/login", data).pipe(
+        const data = {LoginType: 0, Value: login, Password: password};
+        return  this.http.post<any>("/api/Account/login", data).pipe(
             tap(response=>{
                 if (response) {
                     localStorage.setItem('jwt_token', response.token);
