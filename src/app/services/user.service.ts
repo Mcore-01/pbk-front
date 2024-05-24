125395707b25084a2aea4d9035ef22b573b20527
Mcore-01 <dassd2123@gmail.com>
2024-05-24 10:10:40 +0500

    feat: добавил crud для операций в сервисе

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IPaginatedData} from "../response-models/PaginatedData";
 import {IUserCard} from "../models/usercard";
+import {IOperation} from "../models/operation";
+import {IBank} from "../models/bank";
+import {tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +16,29 @@ export class UserService {
   getUserCards(){
     let url = '/api/UserCard';
     return this.http.get<IUserCard[]>(url);
+  }
+
+  getUserOperations(){
+    let url = '/api/Operation';
+    return this.http.get<IOperation[]>(url);
+  }
+
+  getUserOperation(id: number){
+    let url = `/api/Operation/${id}`;
+    return this.http.get<IOperation>(url);
+  }
+
+  createUserOperation(data: IOperation){
+    return this.http.post("/api/Operation", data);
+  }
+  updateUserOperation(data: IBank){
+    return this.http.put("/api/Operation", data);
+  }
+  deleteUserOperation(id: number){
+    return this.http.delete(`/api/Operation/${id}`);
+  }
+
+  deleteListUserOperation(ids: number[]){
+    return this.http.request( 'delete',`/api/Operation/list`, {body: JSON.stringify(ids)});
   }
 }
